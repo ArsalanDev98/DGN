a7a19742ddc05520024476641206ed93728304d4
ArsalanKhalid98 <ArsalanKhalid98@outlook.com>
2024-09-05 18:45:38 +0200

    Hamburger menu added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import styles from './Navbar.module.css';
-import logo from '/assets/NewWeb/Logo/DGN-navbar-logo.svg';
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import logo from "/assets/NewWeb/Logo/DGN-navbar-logo.svg";
 
 const Navbar: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -29,31 +29,52 @@ const Navbar: React.FC = () => {
           </Link>
         </div>
 
-        <div className={styles.navCenter}>
+        <div
+          className={`${styles.navCenter} ${menuOpen ? styles.showMenu : ""}`}
+        >
           <ul className={styles.navLinks}>
             <li className={styles.navItem}>
-              <Link to="/" className={styles.navLink}>Home</Link>
+              <Link to="/" className={styles.navLink}>
+                Home
+              </Link>
             </li>
             <li className={styles.navItem}>
-              <Link to="/vision" className={styles.navLink}>Vision</Link>
+              <Link to="/vision" className={styles.navLink}>
+                Vision
+              </Link>
             </li>
             <li className={styles.navItem}>
-              <Link to="/blog" className={styles.navLink}>Blog</Link>
+              <Link to="/blog" className={styles.navLink}>
+                Blog
+              </Link>
             </li>
             <li className={styles.navItem}>
-              <Link to="/how-to-use" className={styles.navLink}>How To Use</Link>
+              <Link to="/how-to-use" className={styles.navLink}>
+                How To Use
+              </Link>
             </li>
             <li className={styles.navItem}>
-              <Link to="/roadmap" className={styles.navLink}>Roadmap</Link>
+              <Link to="/roadmap" className={styles.navLink}>
+                Roadmap
+              </Link>
             </li>
             <li className={styles.navItem}>
-              <Link to="/incentive" className={styles.navLink}>Incentive Program</Link>
+              <Link to="/incentive" className={styles.navLink}>
+                Incentive Program
+              </Link>
             </li>
           </ul>
         </div>
 
         <div className={styles.buttonContainer}>
-          <Link to="https://organization-ows.gitbook.io/usdbetz-documents" target="_blank" rel="noopener noreferrer" className={styles.navButton}>Whitepaper</Link>
+          <Link
+            to="https://organization-ows.gitbook.io/usdbetz-documents"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.navButton}
+          >
+            Whitepaper
+          </Link>
         </div>
       </nav>
     </section>
