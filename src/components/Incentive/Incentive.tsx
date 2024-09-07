41b5356a7d6b09602b41745dc84ad04d5e655ee4
ArsalanKhalid98 <ArsalanKhalid98@outlook.com>
2024-09-07 19:03:27 +0200

    A lot of updates

--- src/components/Incentive/Incentive.tsx
+++ src/components/Incentive/Incentive.tsx
@@ -5,16 +5,20 @@ import TeamInfo from "../Team/TeamInfo";
 const Incentive: React.FC = () => {
   return (
     <section className={styles.incentiveSection}>
-      <div className={styles.incentiveContainer}>
-        <h1>Incentive Program</h1>
-        <div className={styles.textContainer}>
+      <div className="commonContainer">
+        <div className={styles.incentiveContainer}>
+          <h1>Incentive Program</h1>
+          <h2 className={styles.subTitle}>Earn 20% Commission</h2>
           <p className={styles.introText}>
             Refer a group and earn 20% of all commissions from its profits for
             life. Group owners also receive 20% of profits from groups they own
             with the bot. Contact us if you're interested in partnering.
           </p>
+          <div className={styles.incentiveImageContainer}>
+            <img src="/assets/NewWeb/Assets/Incentive.png" alt="Incentive" />
+          </div>
+          <TeamInfo />
         </div>
-        <TeamInfo />
       </div>
     </section>
   );
