35921122165e1ea44f3cdc4ac8671fe3a91ec532
ArsalanKhalid98 <ArsalanKhalid98@outlook.com>
2024-09-10 23:13:47 +0200

    removed unused

--- src/pages/IncentivePage.tsx
+++ src/pages/IncentivePage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Newsletter from "../components/Newsletter/Newsletter";
 import Community from "../components/Community/Community";
 import Incentive from "../components/Incentive/Incentive";
 
