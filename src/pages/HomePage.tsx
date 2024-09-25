6ef141f1d83982f504a2291ecaaa43c1d445d93f
ArsalanKhalid98 <ArsalanKhalid98@outlook.com>
2024-09-25 22:55:51 +0200

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import BetLounge from "../components/BetLounge/BetLounge";
 import AboutUs from "../components/AboutUs/AboutUs";
 import Faq from "../components/Faq/Faq";
+import Community from "../components/Community/Community";
 
 const Team = React.lazy(() => import("../components/Team/Team"));
 
@@ -13,6 +14,7 @@ const HomePage: React.FC = () => {
       <Faq />
       <Suspense fallback={<div>Loading...</div>}>
         <Team />
+        <Community />
       </Suspense>
     </>
   );
